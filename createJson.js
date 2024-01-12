1b137f7bba56ec34380fb34678a7a2919190c535
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2024-01-12 19:49:56 +0800

    01.12

--- createJson.js
+++ createJson.js
@@ -1,16 +1,36 @@
 const express = require('express');
-
 const port = 8000;
 const app = express();
+const cors = require('cors');
+const { readUsers, readProducts, readCars, createUser } = require('./index.js')
+// const readProducts = require('./index.js');
 
-app.get('/', (req, res) => {
-    res.send("HELlo World")
+
+app.use(cors());
+
+app.get('/users', (req, res) => {
+    const users = readUsers();
+    res.send(users);
 });
-app.get('/express', (req, res) => {
-    res.send("HELlo express")
+app.get('/products', (req, res) => {
+    const products = readProducts();
+    res.send(products);
 });
-app.post('/express', (req, res) => {
-    res.send("Success")
+app.get('/cars', (req, res) => {
+    const cars = readCars();
+    res.send(cars);
+});
+
+app.post('/users', (req, res) => {
+    const user = createUser({
+        "id": 6,
+        "first_name": "Serik",
+        "last_name": "KH",
+        "email": "cstrutton3@technorati.com",
+        "gender": "Male",
+        "ip_address": "6.177.254.244"
+    });
+    res.send(user);
 });
 
 app.listen(port, () => {
--- index.js
+++ index.js
@@ -1,57 +1,62 @@
 const http = require('http');
 const fs = require('fs');
+const { json } = require('express');
 
-const PORT = 8080;
+// const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/users') {
-        readUsers(res);
-    } else if (url === '/products') {
-        readProducts(res);
-    } else if (url === '/cars') {
-        readCars(res);
-    } else if (url === '/') {
-        readUsers(res);
-    }
-    else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Error Not Found');
-    }
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     if (url === '/users') {
+//         readUsers(res);
+//     } else if (url === '/products') {
+//         readProducts(res);
+//     } else if (url === '/cars') {
+//         readCars(res);
+//     } else if (url === '/') {
+//         readUsers(res);
+//     }
+//     else {
+//         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//         res.end('Error Not Found');
+//     }
 
 
-}).listen(PORT);
+// }).listen(PORT);
 
-const readUsers = (res) => {
-    fs.readFile('USERS.json', 'utf8', (err, data) => {
-        if (err) {
-            return console.error(err);
-        }
-        const users = JSON.parse(data);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
-    })
+const readUsers = () => {
+    const data = fs.readFileSync('USERS.json', 'utf8',)
+    const users = JSON.parse(data);
+    return users
 };
 
-const readProducts = (res) => {
-    fs.readFile('PRODUCTS.json', 'utf8', (err, data) => {
-        if (err) {
-            return console.error(err)
-        }
+const readProducts = () => {
+    try {
+        const data = fs.readFileSync('PRODUCTS.json', 'utf8')
         const products = JSON.parse(data);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(products));
-    })
+        return products
+    } catch (err) {
+        console.error(err);
+        return null
+    }
 }
-const readCars = (res) => {
-    fs.readFile('CARS.json', 'utf8', (err, data) => {
-        if (err) {
-            return console.error(err)
-        }
-        const cars = JSON.parse(data);
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(cars));
-    })
-}
-console.log("Application running at: http://localhost:" + PORT)
 
+const readCars = () => {
+    const data = fs.readFileSync('CARS.json', 'utf8');
+    const cars = JSON.parse(data)
+    return cars
+
+}
+
+const createUser = (newUser) => {
+    const data = fs.readFileSync('USERS.json', 'utf8')
+    const users = JSON.parse(data);
+
+    users.push(newUser)
+
+    fs.writeFileSync('USERS.json', JSON.stringify(users))
+}
+// console.log("Application running at: http://localhost:" + PORT)
+
+// exports.readUsers = readUsers;
+
+module.exports = { readUsers, readProducts, readCars, createUser }
